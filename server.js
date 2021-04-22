456001e47fb0eb4653830e073a049312425043b0
markosanchez800 <markosanchez800@gmail.com>
2021-04-22 12:56:59 -0700

    cleaned up file placement to hopefully ease access for going between files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
+const mainDirectory = path.join(__dirname,'/public');
 
 const app = express();
 const PORT = 5600;
@@ -10,12 +10,12 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'index.html')));
+app.get('/',(req,res)=>res.sendFile(path.join(mainDirectory,'index.html')));
 
-app.get('/notes',(req,res)=>res.sendFile(path.join(__dirname,'notes.html')));
-
+app.get("/notes",(req,res)=>res.sendFile(path.join(mainDirectory,'notes.html')));
+//why is app not registering notes or db file?? figure out
 app.get('/api/notes',(req,res)=>res.sendFile(path.join(__dirname,'/db/db.json')));
 
 app.get('/api/notes/:id',function(req,res){
