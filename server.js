608043a62ccac212ad9297e15888e9d3d8d2c6b8
markosanchez800 <markosanchez800@gmail.com>
2021-04-22 17:25:12 -0700

    changed port to fix connection error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const {v4:uuidv4} = require('uuid');
 //const mainDirectory = path.join(__dirname,'/public');
 
 const app = express();
-const PORT = 5600;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
